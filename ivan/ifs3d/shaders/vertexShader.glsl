c105742a7f406d040b5ad8ef221b1a6166dc7ae9
unknown <Administrator@.(none)>
2010-03-07 23:28:11 -0800

    Ponovo vratio shader koji bio zakomentiran.

--- ivan/ifs3d/shaders/vertexShader.glsl
+++ ivan/ifs3d/shaders/vertexShader.glsl
@@ -4,13 +4,13 @@ varying float Distance;
 
 void main()
 { 
-//	vec4 v = vec4(gl_Vertex);
-//	float dx = v.x - CameraPosition.x;
-//	float dy = v.y - CameraPosition.y;
-//	float dz = v.z - CameraPosition.z;
-//	Distance = clamp((dx*dx)+(dy*dy)+(dz*dz), 0.0, FadeOffDist);
+	vec4 v = vec4(gl_Vertex);
+	float dx = v.x - CameraPosition.x;
+	float dy = v.y - CameraPosition.y;
+	float dz = v.z - CameraPosition.z;
+	Distance = clamp((dx*dx)+(dy*dy)+(dz*dz), 0.0, FadeOffDist);
 
-	gl_FrontColor = gl_Color; // * (1-Distance/FadeOffDist);
+	gl_FrontColor = gl_Color * (1-Distance/FadeOffDist);
 	gl_FrontColor.a = 0.5;
-	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	gl_Position = gl_ModelViewProjectionMatrix * v;
 }
